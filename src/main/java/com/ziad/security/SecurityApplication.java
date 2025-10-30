3b4ba9acc46d4f610d034d7c1d464168179a61f5
Omar Ziad <o.ziad@outlook.com>
2025-10-30 00:36:11 -0400

    feat(token): implement jwt token validation service

--- src/main/java/com/ziad/security/SecurityApplication.java
+++ src/main/java/com/ziad/security/SecurityApplication.java
@@ -2,12 +2,13 @@ package com.ziad.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(SecurityApplication.class, args);
+    }
 
 }
